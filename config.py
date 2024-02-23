cb5e7186845a4cc77664d8658f8a4680ce584974
hectorog6612 <159784158+hectorog6612@users.noreply.github.com>
2024-02-23 23:07:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6856050563:AAEqixji4Wxqx-lZ25TUeXskvisbrmZM2n4"
-    API_ID = "23282918"
-    API_HASH = "ccf2a6664d1ae478b8c556000f616120"
+    BOT_TOKEN = "6990903871:AAFuHNio4mZhYzYObfpyvyaxcE_stOb3yyo"
+    API_ID = "10608382"
+    API_HASH = "ea04136e7acb517f48ccf8425ca99243"
     AUTH_USERS = '5911618569'
     
